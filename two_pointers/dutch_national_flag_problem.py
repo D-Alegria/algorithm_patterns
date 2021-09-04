81ed3b66eabb821ec5336982cf50044dc46c3ded
Oladugba Demilade <demiladeoladugba@gmail.com>
2021-09-04 10:22:20 +0100

    added dutch_national_flag_problem.py

--- two_pointers/dutch_national_flag_problem.py
+++ two_pointers/dutch_national_flag_problem.py
@@ -13,26 +13,43 @@
     Input: [1, 0, 2, 1, 0]
     Output: [0 0 1 1 2]
         *
-    0   0   1   1   2
-            ^
+    0   1   0   1   2
+                ^
     Example 2:
     Input: [2, 2, 0, 1, 2, 0]
     Output: [0 0 1 2 2 2 ]
 """
 
 
-def dutch_national_flag(arr: [int]) -> [int]:
+def dutch_national_flag1(arr: [int]) -> [int]:
     size = len(arr) - 1
 
-    for i in range(size):
+    for i in range(size):  # N
         front = i + 1
         back = i
-        while front <= size and back >= 0:
+        while front <= size and back >= 0:  # N
             if arr[back] > arr[front]:
                 arr[front], arr[back] = arr[back], arr[front]
                 back, front = back - 1, front - 1
             else:
                 break
+    return arr
+
+
+def dutch_national_flag(arr: [int]) -> [int]:
+    back, front = 0, len(arr) - 1
+    i = 0
+    while i <= front:  # N
+        if arr[i] == 0:
+            arr[back], arr[i] = arr[i], arr[back]
+            i += 1
+            back += 1
+        elif arr[i] == 2:
+            arr[front], arr[i] = arr[i], arr[front]
+            front -= 1
+        else:
+            i += 1
+
     return arr
 
 
