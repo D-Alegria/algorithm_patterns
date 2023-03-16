94eeb96d9ea3115569979a9b365847ee6c63d64b
Demilade Oladugba <demiladeoladugba@gmail.com>
2023-03-16 09:03:13 +0000

    added matchsticks_to_square.py

---
+++ backtracking/matchsticks_to_square.py
@@ -0,0 +1,37 @@
+def matchstick_to_square(matchsticks):
+    n = len(matchsticks)
+    if n < 4:
+        return False
+    total = sum(matchsticks)
+    if total < 4 and total % 4 != 0:
+        return False
+    matchsticks.sort(reverse=True)
+    one_side_length = total // 4
+    print(total, one_side_length)
+
+    def backtracking(sticks):
+        print(sticks)
+        if len(sticks) == 0:
+            return True
+
+        if sticks[0] == one_side_length:
+            return backtracking(sticks[1:])
+        elif sticks[0] < one_side_length:
+            for i in range(1, len(sticks)):
+                temp = sticks[:]
+                temp[0] += temp[i]
+                if backtracking(temp[:i] + temp[i + 1:]):
+                    return True
+            return False
+        else:
+            return False
+
+    return backtracking(matchsticks)
+
+
+if __name__ == '__main__':
+    print(matchstick_to_square([1, 1, 2, 2, 2]))
+    print(matchstick_to_square([3, 3, 3, 3, 4]))
+    print(matchstick_to_square([1, 1, 1, 2, 1]))
+    print(matchstick_to_square([3, 4, 4, 1, 2, 2]))
+    print(matchstick_to_square([5, 6, 1, 1, 2, 2]))
