e8c882779ccffebbcd237db19f83a15fc3c80db2
Demilade Oladugba <demiladeoladugba@gmail.com>
2023-03-17 07:35:35 +0000

    updated matchsticks_to_square.py

--- backtracking/matchsticks_to_square.py
+++ backtracking/matchsticks_to_square.py
@@ -27,9 +27,40 @@ def matchstick_to_square(matchsticks):
     return backtracking(matchsticks)
 
 
+def matchstick_to_square_optimized(matchsticks):
+    n = len(matchsticks)
+    if n < 4:
+        return False
+    total = sum(matchsticks)
+    if total % 4 != 0:
+        return False
+
+    matchsticks.sort(reverse=True)
+    sides = [0] * 4
+
+    def backtracking(index):
+        if index == n:
+            return True
+
+        for j in range(4):
+            if sides[j] + matchsticks[index] <= total // 4:
+                sides[j] += matchsticks[index]
+                if backtracking(index + 1):
+                    return True
+                sides[j] -= matchsticks[index]
+        return False
+    return backtracking(0)
+
+
 if __name__ == '__main__':
     print(matchstick_to_square([1, 1, 2, 2, 2]))
     print(matchstick_to_square([3, 3, 3, 3, 4]))
     print(matchstick_to_square([1, 1, 1, 2, 1]))
     print(matchstick_to_square([3, 4, 4, 1, 2, 2]))
     print(matchstick_to_square([5, 6, 1, 1, 2, 2]))
+    print("*" * 10 , "Optimized")
+    print(matchstick_to_square_optimized([1, 1, 2, 2, 2]))
+    print(matchstick_to_square_optimized([3, 3, 3, 3, 4]))
+    print(matchstick_to_square_optimized([1, 1, 1, 2, 1]))
+    print(matchstick_to_square_optimized([3, 4, 4, 1, 2, 2]))
+    print(matchstick_to_square_optimized([5, 6, 1, 1, 2, 2]))
